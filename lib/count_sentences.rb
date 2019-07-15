b2a51145ea4b2c0f5398d9bde355fafbb7ac1893
alejandra cantu <alejandra.cantu13@gmail.com>
2019-07-15 16:24:31 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,9 +1,21 @@
 require 'pry'
 
+
 class String
 
+attr_accessor :name
+
+  def initialize(name)
+    puts "Hi, my name is #{self}!"
+  end
+
+
   def sentence?
-    
+  if self.end_with?(".")
+  return true
+else
+  false
+    end
   end
 
   def question?
